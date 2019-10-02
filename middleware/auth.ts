c271cd67f08ad4bfe1bc0eef6ea4f95fac6984dd
Jose Palma <jcpalma@jcpalmamac.local>
2019-10-01 23:48:56 -0600

    Configuración del login y JWT.
    1. Creación de la ruta /login y su controller.
    2. Encriptación de constraseña con bcrypt.
    3. Creación de la función middleware auth.

---
+++ middleware/auth.ts
@@ -0,0 +1,35 @@
+
+import { Request, Response, NextFunction } from 'express';
+import { SEED } from '../config/config';
+import { IUser } from '../src/models/user-model';
+import jwt from 'jsonwebtoken';
+
+declare global {
+    namespace Express {
+        interface Request {
+            authUser?: IUser
+        }
+    }
+}
+
+/**
+ * * Borrar un usuario.
+ * @param req request header
+ * @param res response body.
+ * @param next 
+ */
+export function auth(req: Request, res: Response, next: NextFunction) {
+    const token = req.query.token;
+    // verifica el token
+    jwt.verify(token, SEED, (err: any, decoded: any) => {
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                message: 'Token no valido.',
+                errors: err
+            });
+        }
+        req.authUser = decoded.user;
+        next();
+    });
+}
